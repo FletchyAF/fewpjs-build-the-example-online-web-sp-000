095e914a8c7044effe066072a87a868d7e0550ef
Joshua Fletcher <joshua.f1998@gmail.com>
2021-06-02 18:57:19 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function likeHeart(e) {
     .catch(function() {
       const element = document.getElementById("modal");
       element.classList.remove("hidden");
-      element.innerHTML = "Random server error. Try again.";
+      element.innerHTML = [reject];
       setTimeout(function() {
         document.getElementById("modal").classList.add("hidden")
       }, 3000)
